b871c5a66290aede65c0695ad0d9ce705bc4d2b2
Davis C <davisclaib@gmail.com>
2025-09-23 19:08:31 -0500

    Simplified orchestration

--- src/slp2mp4/orchestrator.py
+++ src/slp2mp4/orchestrator.py
@@ -1,12 +1,16 @@
 # Commonizes the batching / concatenating of slippi files
-# There are to threads here:
-#   1. The "render" thread, which runs dolphin / does frame dumps
-#   2. The "concat" thread, which contatenates frame-dumps into a single mp4
+#
+# This renders by "set," which will be slightly slower on average than rendering
+# all videos then concat-ing when the set is finished, but has a few upsides:
+#
+#   1. Reduces memory usage
+#   2. Simplifies implementation
 
-import multiprocessing
-import os
+import concurrent.futures
+import dataclasses
+import enum
+
 import pathlib
-import queue
 import tempfile
 
 import slp2mp4.ffmpeg as ffmpeg
@@ -14,84 +18,28 @@ import slp2mp4.video as video
 from slp2mp4.output import Output
 
 
-def _render(conf, slp_queue, video_queue):
-    while True:
-        data = slp_queue.get()
-        if data is None:
-            break
-        output_name, slp_path = data
-        tmp = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
-        video.render(conf, slp_path, pathlib.Path(tmp.name))
-        tmp.close()
-        video_queue.put((output_name, slp_path, tmp.name))
+def render_and_concat(executor: concurrent.futures.Executor, conf: dict, output: Output):
+    futures = {i: executor.submit(render, conf, i) for i in output.inputs}
+    concurrent.futures.wait(futures.values())
+    tmp_paths = [futures[i].result() for i in output.inputs]
+    concat(conf, output.output, tmp_paths)
 
+def render(conf: dict, slp_path: pathlib.Path):
+    tmp = tempfile.NamedTemporaryFile(suffix=".mp4", delete=False)
+    tmp_path = pathlib.Path(tmp.name)
+    video.render(conf, slp_path, tmp_path)
+    tmp.close()
+    return tmp_path
 
-def _concat(conf, video_queue, outputs):
+def concat(conf: dict, output_path: pathlib.Path, renders: list[pathlib.Path]):
     Ffmpeg = ffmpeg.FfmpegRunner(conf)
-    mp4s = {}
-    while True:
-        data = video_queue.get()
-        if data is None:
-            break
-        output_name, slp_path, mp4_path = data
-        if output_name not in mp4s:
-            mp4s[output_name] = {}
-        mp4s[output_name][slp_path] = mp4_path
-        output = list(filter(lambda o: o.output == output_name, outputs))[0]
-        if len(mp4s[output_name]) < len(output.inputs):
-            continue
-        tmpfiles = [pathlib.Path(mp4s[output_name][slp]) for slp in output.inputs]
-        output_name.parent.mkdir(parents=True, exist_ok=True)
-        Ffmpeg.concat_videos(tmpfiles, output_name)
-        for tmp in tmpfiles:
-            os.unlink(tmp)
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    Ffmpeg.concat_videos(renders, output_path)
+    for render in renders:
+        render.unlink()
 
-
-def run(conf, outputs: list[Output]):
+def run(conf: dict, outputs: list[Output]):
     num_procs = conf["runtime"]["parallel"]
-    slp_queue = multiprocessing.Queue()
-    video_queue = multiprocessing.Queue()
-    slp_pool = multiprocessing.Pool(
-        num_procs,
-        _render,
-        (
-            conf,
-            slp_queue,
-            video_queue,
-        ),
-    )
-    video_pool = multiprocessing.Pool(
-        1,
-        _concat,
-        (
-            conf,
-            video_queue,
-            outputs,
-        ),
-    )
-
-    for output in outputs:
-        for slp in output.inputs:
-            slp_queue.put(
-                (
-                    output.output,
-                    slp,
-                )
-            )
-
-    for i in range(num_procs):
-        slp_queue.put(None)
-
-    slp_queue.close()
-    slp_queue.join_thread()
-
-    slp_pool.close()
-    slp_pool.join()
-
-    video_queue.put(None)
-
-    video_queue.close()
-    video_queue.join_thread()
-
-    video_pool.close()
-    video_pool.join()
+    with concurrent.futures.ProcessPoolExecutor(num_procs) as executor:
+        for output in outputs:
+            render_and_concat(executor, conf, output)
